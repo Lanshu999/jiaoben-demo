9e4a0723693ba9cd53f5467937c77fc0b5d4ff74
Lanshu <Test1@lanshu.com>
2023-09-15 15:33:01 +0800

    create temp.js

---
+++ temp.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         hello world
+// @namespace    test@lanshu
+// @version      0.1
+// @description  new script
+// @author       lanshu
+// @match        *
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=chrxw.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+    console.log("hello world");
+})();
